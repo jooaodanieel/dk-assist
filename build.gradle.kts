0dba5e6281fd8453880e7b2b0fff466452c79faf
João Daniel <jotaf.daniel@gmail.com>
2022-07-09 12:15:21 -0300

    init
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+val kotlinx_serialization_version: String by project
+
+plugins {
+    kotlin("multiplatform") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+}
+
+group = "me.joaofran"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    sourceSets {
+        val nativeMain by getting
+        val nativeTest by getting
+
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_version")
+            }
+        }
+    }
+}
